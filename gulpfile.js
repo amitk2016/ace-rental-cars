716cbf6bb40fcb8a9569d9fa2b7f11cca85684d9
Amit Kumar <amit.k03377@gmail.com>
2018-10-16 13:10:59 +1300

    Sample page left top navbar html is done with left section of leftnavbar css

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const autoprefixer = require("gulp-autoprefixer");
 // Compile Sass & Inject Into Browser
 gulp.task("sass", function() {
   return gulp
-    .src(["src/scss/*.scss"])
+    .src(["src/scss/**/*.scss"])
     .pipe(sass())
     .pipe(
       autoprefixer({
@@ -24,7 +24,7 @@ gulp.task("serve", ["sass"], function() {
     server: "./src"
   });
 
-  gulp.watch(["src/scss/*.scss"], ["sass"]);
+  gulp.watch(["src/scss/**/*.scss"], ["sass"]);
   gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
